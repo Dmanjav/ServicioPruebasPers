76086913084ca60664987415ea8f3bd2b8933aae
Dmanjav <94866755+Dmanjav@users.noreply.github.com>
2023-01-20 18:53:44 -0600

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import { CssBaseline,ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
+import { lightTheme } from './themes/lightTheme'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme = {lightTheme}>
+        <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material"
+
+const HomePage=() => {
+  return (
+    <div><Button color = "primary">Press</Button></div>
+  )
+}
+export default HomePage
