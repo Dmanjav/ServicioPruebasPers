0505fa50602396e44b1330b4fdc829dcd8ec40ce
Dmanjav <A01753486@tec.mx>
2023-01-22 13:11:26 -0600

    boton de config
    tiene un menu, recuerda trabajar en pruebas 2 pq mate al otro

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-/>
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
 
       </Head>
       <body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,84 @@
-import { Button } from "@mui/material"
+/*import { Button } from "@mui/material"
 
 const HomePage=() => {
   return (
     <div><Button color = "primary">Press</Button></div>
   )
 }
-export default HomePage
+export default HomePage*/
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import LogoutIcon from '@mui/icons-material/Logout';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+
+export default function App() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+      <SettingsIcon />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <PermIdentityIcon />
+          Perfil     
+        </MenuItem>
+
+        <MenuItem onClick={handleClose}>
+          <ShoppingCartIcon />
+          Mi carrito
+        </MenuItem>
+
+        <MenuItem onClick={handleClose}>
+          <CalendarMonthIcon />
+          Historial de compras     
+        </MenuItem>
+
+        <MenuItem onClick={handleClose}>
+          <CreditCardIcon />
+          Métodos de pago
+        </MenuItem>
+
+        <MenuItem onClick={handleClose}>
+          <ShoppingCartIcon />
+          Carrito de compra
+        </MenuItem>
+        
+        <MenuItem onClick={handleClose}>
+          <LogoutIcon />
+          Cerrar sesión
+        </MenuItem>
+        
+      </Menu>
+    </div>
+  );
+}
